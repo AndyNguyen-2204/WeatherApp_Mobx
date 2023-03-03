326203adc5dec36a19ed0d57f7f2a05c7e03fcfe
Lavie-3107 <nguyenanhtu2204tb@gmail.com>
2023-03-03 14:50:28 +0700

    fix bug and add input search city

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Home from './screen/home/Home';
-import { useObserver } from "mobx-react";
+import { observer } from 'mobx-react';
 import TabBottom from './src/components/navigation/tabBottom/tabBottom';
-export default function App() {
+  const App=observer(()=>{
   return(
      <TabBottom/>
   );
-}
-
+})
+export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
